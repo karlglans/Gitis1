7b60657db148be19dfaa1b6cb22a268922a50b06
Isak Glans <isak.glans@unibase.se>
2023-03-28 11:56:36 +0200

    Feature 2 branch commit nr 2

--- Gitis1/Program.cs
+++ Gitis1/Program.cs
@@ -9,6 +9,7 @@
             Console.WriteLine("Commit 4");
             Console.WriteLine("Commit 5");
             Console.WriteLine("Feature 2 branch gjorde en ändring. Klockan 11:55.");
+            Console.WriteLine("Feature 2 branch gjorde en ändring. Klockan 11:56.");
         }
     }
 }
